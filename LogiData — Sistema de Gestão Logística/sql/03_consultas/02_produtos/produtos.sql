2da81bb9b7be0d8b96e7d6309b8e7d3c840816ef
Hiago Mateus <andradedefariahiagomateus@gmail.com>
2026-09-03 19:18:54 -0300

    atualiza consultas sql de produtos
    atualiza as consultas de produtos para utilizar as funções min e max,
    ajustando as consultas de maior preço, menor preço e maior peso.

--- LogiData — Sistema de Gestão Logística/sql/03_consultas/02_produtos/produtos.sql
+++ LogiData — Sistema de Gestão Logística/sql/03_consultas/02_produtos/produtos.sql
@@ -4,10 +4,10 @@ select count(id_produto) as qtd_cadastro_produtos from produtos;
 select categoria, count (id_produto) from produtos GROUP by categoria;
 
 
-select nome, categoria, preco from produtos order by preco desc limit 5;
+select max(preco) as maior_preco from produtos;
 
 
-select nome, categoria, preco from produtos order by preco asc limit 5;
+select min(preco) as menor_preco from produtos;
 
 
-SELECT nome, categoria, peso_kg from produtos ORDER by peso_kg desc limit 3;
+select max(peso_kg) as maior_peso from produtos;
