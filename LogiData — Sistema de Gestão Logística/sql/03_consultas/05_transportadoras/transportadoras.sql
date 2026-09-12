1299ce44ac238fc1e35dfa867e22fbb5ea580728
Hiago Mateus <andradedefariahiagomateus@gmail.com>
2026-09-11 21:01:30 -0300

    completa consultas de transportadoras
    Adiciona consultas para identificar as transportadoras com maior quantidade de entregas concluídas e atrasadas.

--- LogiData — Sistema de Gestão Logística/sql/03_consultas/05_transportadoras/transportadoras.sql
+++ LogiData — Sistema de Gestão Logística/sql/03_consultas/05_transportadoras/transportadoras.sql
@@ -1,9 +1,11 @@
-SELECT COUNT(id_transportadora) AS qtd_transportadoras FROM transportadoras;
+select count(id_transportadora) as qtd_transportadoras from transportadora;
 
-SELECT transportadora_id, COUNT(id_entrega) AS qtd_entregas FROM entregas GROUP BY transportadora_id;
+select transportadora_id, count(id_entrega) as qtd_entregas from entregas group by transportadora_id;
 
-SELECT transportadora_id, COUNT(id_entrega) AS qtd_entregas FROM entregas GROUP BY transportadora_id ORDER BY qtd_entregas DESC LIMIT 1;
+select transportadora_id, count(id_entrega) as qtd_entregas from entregas group by transportadora_id order by qtd_entregas desc limit 1;
 
-SELECT transportadora_id, COUNT(id_entrega) AS qtd_entregas FROM entregas GROUP BY transportadora_id ORDER BY qtd_entregas ASC LIMIT 1;
+select transportadora_id, count(id_entrega) as qtd_entregas from entregas group by transportadora_id order by qtd_entregas asc limit 1;
 
-SELECT transportadora_id, COUNT(id_entrega) AS qtd_entregas FROM entregas GROUP BY transportadora_id;
+select transportadora_id, count(id_entrega) as qtd_entregas_concluidas from entregas where status = 'Entregue' group by transportadora_id order by qtd_entregas_concluidas desc limit 1;
+
+select transportadora_id, count(id_entrega) as qtd_entregas_atrasadas from entregas where data_entrega > data_prevista group by transportadora_id order by qtd_entregas_atrasadas desc limit 1;
