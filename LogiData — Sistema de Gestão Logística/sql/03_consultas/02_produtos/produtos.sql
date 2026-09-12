3f22287f1d951eb320a76aa13b4d1d4705690a16
Hiago Mateus <andradedefariahiagomateus@gmail.com>
2026-09-11 21:23:55 -0300

    completa consultas de produtos
    Adiciona consultas para identificar os produtos mais
    solicitados, mais presentes em pedidos, com maior
    movimentação financeira e o peso total dos produtos
    presentes nos pedidos.

--- LogiData — Sistema de Gestão Logística/sql/03_consultas/02_produtos/produtos.sql
+++ LogiData — Sistema de Gestão Logística/sql/03_consultas/02_produtos/produtos.sql
@@ -1,13 +1,17 @@
 select count(id_produto) as qtd_cadastro_produtos from produtos;
 
-
-select categoria, count (id_produto) from produtos GROUP by categoria;
-
+select categoria, count(id_produto) as qtd_produtos from produtos group by categoria;
 
 select max(preco) as maior_preco from produtos;
 
-
 select min(preco) as menor_preco from produtos;
 
+select pr.id_produto, pr.nome, sum(ip.quantidade) as qtd_unidades from produtos pr inner join itens_pedido ip on pr.id_produto = ip.produto_id group by pr.id_produto, pr.nome order by qtd_unidades desc;
+
+select pr.id_produto, pr.nome, count(distinct ip.pedido_id) as qtd_pedidos from produtos pr inner join itens_pedido ip on pr.id_produto = ip.produto_id group by pr.id_produto, pr.nome order by qtd_pedidos desc;
+
+select pr.id_produto, pr.nome, sum(ip.quantidade * ip.preco_unitario) as valor_total_movimentado from produtos pr inner join itens_pedido ip on pr.id_produto = ip.produto_id group by pr.id_produto, pr.nome order by valor_total_movimentado desc;
 
 select max(peso_kg) as maior_peso from produtos;
+
+select sum(ip.quantidade * pr.peso_kg) as peso_total_kg from itens_pedido ip inner join produtos pr on ip.produto_id = pr.id_produto;
