ea6371bf221f9fb94e3ffd36408ae063c05ca6a3
Hiago Mateus <andradedefariahiagomateus@gmail.com>
2026-09-11 21:00:18 -0300

    adiciona consulta de quantidade de pedidos por cliente
    Adiciona a consulta que identifica os clientes que realizaram a maior quantidade de pedidos, completando as consultas da entidade clientes.

--- LogiData — Sistema de Gestão Logística/sql/03_consultas/01_clientes/clientes.sql
+++ LogiData — Sistema de Gestão Logística/sql/03_consultas/01_clientes/clientes.sql
@@ -1,16 +1,11 @@
-SELECT count(id_cliente) as qtd_cadastros from clientes;
+select count(id_cliente) as qtd_cadastros from clientes;
 
+select estado, count(*) as quantidade_clientes from clientes group by estado;
 
-SELECT estado, COUNT(*) AS quantidade_clientes FROM clientes GROUP BY estado;
+select cidade, count(*) as qtd_clientes from clientes group by cidade order by qtd_clientes desc;
 
+select c.nome, count(p.id_pedido) as qtd_pedidos from clientes c inner join pedidos p on c.id_cliente = p.cliente_id group by c.id_cliente, c.nome order by qtd_pedidos desc;
 
-SELECT cidade, count (*) as qtd_clientes from clientes GROUP by cidade ORDER by qtd_clientes desc;
+select clientes.nome from clientes left join pedidos on clientes.id_cliente = pedidos.cliente_id where pedidos.id_pedido is null;
 
-
-SELECT clientes.nome FROM clientes
-LEFT JOIN pedidos
-ON clientes.id_cliente = pedidos.cliente_id
-WHERE pedidos.id_pedido IS NULL;
-
-
-SELECT tipo_cliente, COUNT(*) AS qtd_tipo_de_cliente FROM clientes GROUP BY tipo_cliente;
+select tipo_cliente, count(*) as qtd_tipo_de_cliente from clientes group by tipo_cliente;
