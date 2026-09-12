f998b8ed6ceeea7e9a63a0c8b5d62549fd581fe3
Hiago Mateus <andradedefariahiagomateus@gmail.com>
2026-09-11 21:23:21 -0300

    adiciona consulta de valor movimentado por cliente
    Adiciona a consulta que identifica os clientes que
    movimentaram os maiores valores em pedidos.

--- LogiData — Sistema de Gestão Logística/sql/03_consultas/01_clientes/clientes.sql
+++ LogiData — Sistema de Gestão Logística/sql/03_consultas/01_clientes/clientes.sql
@@ -6,6 +6,8 @@ select cidade, count(*) as qtd_clientes from clientes group by cidade order by q
 
 select c.nome, count(p.id_pedido) as qtd_pedidos from clientes c inner join pedidos p on c.id_cliente = p.cliente_id group by c.id_cliente, c.nome order by qtd_pedidos desc;
 
+select c.nome, sum(p.valor_total) as valor_total_movimentado from clientes c inner join pedidos p on c.id_cliente = p.cliente_id group by c.id_cliente, c.nome order by valor_total_movimentado desc;
+
 select clientes.nome from clientes left join pedidos on clientes.id_cliente = pedidos.cliente_id where pedidos.id_pedido is null;
 
 select tipo_cliente, count(*) as qtd_tipo_de_cliente from clientes group by tipo_cliente;
